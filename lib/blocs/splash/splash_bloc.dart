46e2c1ca84831419627bb3538355829088a780f2
Farouq Oyewole <147939016+farouq-oyewole@users.noreply.github.com>
2026-01-13 09:31:28 +0100

    feat: implement splash screen bloc logic

---
+++ lib/blocs/splash/splash_bloc.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'splash_event.dart';
+import 'splash_state.dart';
+
+class SplashBloc extends Bloc<SplashEvent, SplashState> {
+  SplashBloc() : super(SplashInitial()) {
+    on<SplashStarted>(_onSplashStarted);
+  }
+
+  Future<void> _onSplashStarted(
+    SplashStarted event,
+    Emitter<SplashState> emit,
+  ) async {
+    emit(SplashLoading());
+    // Simulate some initialization or check auth status
+    await Future.delayed(const Duration(seconds: 2)); 
+    emit(SplashLoaded());
+  }
+}
