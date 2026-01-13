b8cf1930957ed4a20e85ac720cf93283802fa8ab
Farouq Oyewole <147939016+farouq-oyewole@users.noreply.github.com>
2026-01-13 09:31:57 +0100

    feat: setup main application entry and theme

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+
+import 'package:ashore/core/constants/colors.dart';
+import 'package:ashore/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ashore',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primaryRed,
+          primary: AppColors.primaryRed,
+          brightness: Brightness.light,
+          surface: AppColors.backgroundBeige,
+        ),
+        scaffoldBackgroundColor: AppColors.backgroundBeige,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primaryRed,
+          primary: AppColors.primaryRed,
+          brightness: Brightness.dark,
+          surface: const Color(0xFF121212),
+        ),
+        scaffoldBackgroundColor: const Color(0xFF121212),
+        textTheme: GoogleFonts.poppinsTextTheme(ThemeData.dark().textTheme),
+      ),
+      themeMode: ThemeMode.light, 
+      home: const SplashScreen(),
+    );
+  }
+}
